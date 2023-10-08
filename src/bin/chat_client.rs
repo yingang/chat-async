46d8ea31ba016193f00d2e7ac6d5c47d2000eec6
Gang Yin <1246410+yingang@users.noreply.github.com>
2023-10-08 14:31:42 +0800

    interactive chat client, fix server side in last commit also

--- src/bin/chat_client.rs
+++ src/bin/chat_client.rs
@@ -1,15 +1,38 @@
+use async_std::io::stdin;
 use async_std::net::TcpStream;
 use async_std::prelude::*;
+use async_std::task;
+use std::str;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
     let mut stream = TcpStream::connect("127.0.0.1:1111").await?;
-    stream.write_all(b"hello world").await?;
-    
-    let mut buf = vec![0; 256];
-    let n = stream.read(&mut buf).await?;
 
-    println!("{:?}", &buf[0..n]);
+    let mut incoming = stream.clone();
+    task::spawn(async move {
+        let mut buf = vec![0; 256];
+        while let Ok(n) = incoming.read(&mut buf).await {
+            if n == 0 {
+                break;
+            }
+            if let Ok(msg) = str::from_utf8(&buf[0..n]) {
+                println!("received: {}", msg.trim());
+            }
+        }
+        println!("bye!");   // 加点输出，不然退出了都不知道 
+    });
+
+    let stdin = stdin();
+    loop {
+        let mut line = String::new();
+        stdin.read_line(&mut line).await?;
+        if line.len() == 0 {
+            break;
+        }
+
+        stream.write_all(line.as_bytes()).await?;
+        //println!("sent");
+    };
 
     Ok(())
 }
--- src/bin/chat_server.rs
+++ src/bin/chat_server.rs
@@ -1,25 +1,31 @@
 use async_std::io::{ReadExt, WriteExt};
 use async_std::net::TcpListener;
 use async_std::task;
+use std::str;
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:1111").await?;
     
     loop {
-        let (mut stream, _addr) = listener.accept().await?;
-        task::spawn(async move {
+        let (mut stream, addr) = listener.accept().await?;
+        task::spawn(async move {    // thread::spawn是接收一个闭包，task::spawn是接收一个异步块
             let mut buf = vec![0; 256];
             while let Ok(n) = stream.read(&mut buf).await {
                 if n == 0 {
+                    // buf大小为零的时候，read也会直接返回0
                     break;
                 }
-                println!("{:?}", &buf[0..n]);
-                if let Err(_) = stream.write(&buf[0..n]).await {
-                    break;
+                if let Ok(msg) = str::from_utf8(&buf[0..n]) {
+                    println!("{}: {}", addr, msg.trim());
+                    //println!("sending back...");
+                    if let Err(_) = stream.write_all(&buf[0..n]).await {
+                        break;
+                    }
                 }
-                buf.clear();
+                //println!("waiting...");
             }
+            println!("bye {}!", addr);   // 加点输出，不然退出了都不知道 
         });
     }
 }
